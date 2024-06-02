76c400326c5e3111154381c36f286e66a49376d6
Khanjanpurani <102948217+Khanjanpurani@users.noreply.github.com>
2024-06-02 22:02:02 +0530

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while ! curl -s http://localhost:8080 > /dev/null; do
+    echo "Waiting for Jenkins..."
+    sleep 5
+done
+
+# Download the agent.jar file
+curl -sO http://localhost:8080/jnlpJars/agent.jar
+
+# Run the Jenkins agent
+exec java -jar agent.jar -url http://localhost:8080/ -secret 681bcf2e9a017827f9d239a1d62779188139dfc501900ce3ff1edadac78b2a6b -name "Docker agent" -workDir /home/jenkins/agent
